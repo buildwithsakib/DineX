5f1ec35a6fdf3dd8cbdea67fa7b2de3845e568bb
Sakib Turuk <147747620+buildwithsakib@users.noreply.github.com>
2026-08-23 18:44:49 +0530

    Delete admin directory

--- admin/manager/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/../../config/database.php';
-require_once INCLUDES_PATH . '/auth.php';
-
-logout_user();
-redirect('/dinex/admin/' . ROLE_NAMES[$_SESSION['role_id'] ?? ROLE_SUPER_ADMIN] . '/login.php');
